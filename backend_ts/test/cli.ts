23fb2effc081b72e6d1b02de45c7d1863b0ef139
Rocco A <43506958+GenerelSchwerz@users.noreply.github.com>
2025-02-22 18:46:24 -0500

    made tests common

--- backend_ts/test/cli.ts
+++ backend_ts/test/cli.ts
@@ -1,8 +1,5 @@
 import { createPlugin } from "../src";
 import { Bot, createBot } from "mineflayer";
-import { Vec3 } from "vec3";
-import { PathfinderStopConditions } from "../src/movement/types";
-import { Direction } from "../src/constants";
 import readline from "readline";
 import { handleCommand } from "./common";
 
@@ -17,29 +14,12 @@ bot.once("spawn", () => {
   startCLI();
 });
 
-async function pathfindToCoordinate(coords: Vec3) {
-  const entitiesToCancel = [bot.registry.entitiesByName["zombie"].id];
-  const blocksToCancel = [bot.registry.blocksByName["iron_ore"].id];
-  const biomesToCancel = [bot.registry.biomesByName["minecraft:jungle"].id];
-
-  const cancelOpts = {
-    entities: { ids: entitiesToCancel, radius: 10 },
-    blocks: { types: blocksToCancel, radius: 10 },
-    biomes: { types: biomesToCancel, radius: 32 },
-  };
-
-  const res = await bot.pathfinderAbstract.pathfindToCoordinate(coords, cancelOpts);
-  console.log("RETURNED VALUE:", res);
-}
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-function clearConsole() {
-  process.stdout.write("\u001b[2J\u001b[0;0H");
-}
 
 function generatePrompt(bot: Bot) {
   return `Semantic Steve 1.0.0!

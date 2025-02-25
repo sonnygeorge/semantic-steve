af20511f9f9c78801f20df9ac9f9a871819a4509
sonnygeorge <sonnygeorge5@gmail.com>
2025-02-25 13:36:33 -0500

    Update cli and common with code- (not command-) based function calling

--- backend_ts/test/cli.ts
+++ backend_ts/test/cli.ts
@@ -1,16 +1,16 @@
 import { createPlugin } from "../src";
 import { Bot, createBot } from "mineflayer";
 import readline from "readline";
-import { handleCommand } from "./common";
+import { handleCommand, handleFunctionCall } from "./common";
 
 const bot = createBot({
-  username: "test",
+  username: "SemanticSteve",
 });
 
 bot.once("spawn", () => {
   const semanticSteve = createPlugin({ immediateRadius: 5, nearbyRadius: 128 });
   bot.loadPlugin(semanticSteve);
-  console.log("Bot spawned and ready for interactive CLI commands.");
+  console.log("Bot spawned and ready!");
   startCLI();
 });
 
@@ -21,25 +21,21 @@ const rl = readline.createInterface({
 });
 
 
-function generatePrompt(bot: Bot) {
-  return `Semantic Steve 1.0.0!
-  World State:
-  ${bot.semanticWorld.toString()}
-
-  [${bot.username}] > `;
-}
-
-
-function startCLI() {
-  rl.setPrompt(generatePrompt(bot));
+function startCLI() {  
+  console.log("STATE:", bot.semanticWorld.toString())
+  rl.setPrompt("Please enter your function call (JS syntax): ");
   rl.prompt();
 
-  rl.on("line", async (line) => {
-    await handleCommand(bot, line);
-    rl.setPrompt(generatePrompt(bot));
+  let result: any;
+  rl.on("line", async (line) => {  // E.g., pathfindToCoordinate([-585.5, 68, 54.5])
+    result = await handleFunctionCall(bot, line);
+    console.log("RESULT:", result);
+    console.log("STATE:", bot.semanticWorld.toString())
+    rl.setPrompt("Please enter your function call (JS syntax): ");
     rl.prompt();
   });
 }
+
 
 bot.on("chat", (username, message) => {
   if (username === bot.username) return;

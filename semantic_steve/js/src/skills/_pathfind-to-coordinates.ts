807e6efa2b34ea96275d2fa305971f81ee314e12
sonnygeorge <sonnygeorge5@gmail.com>
2025-04-03 13:50:53 -0400

    Add README.md to skills directory

--- semantic_steve/js/src/skills/_pathfind-to-coordinates.ts
+++ semantic_steve/js/src/skills/_pathfind-to-coordinates.ts
@@ -1,2 +1,16 @@
+import type { Bot } from "mineflayer";
+import { SkillReturn } from "../types";
+import { Vec3 } from "vec3";
+import { Thing } from "src/core/thing";
 
-export async function
+export async function pathfindToCoordinates(
+  bot: Bot,
+  coords: Vec3,
+  stopIfFound: Thing[]
+): Promise<SkillReturn> {
+  return {
+    // TODO
+    resultString: null,
+    envStateIsUpToDate: false,
+  };
+}
--- semantic_steve/js/src/skills/pathfind-to-coordinates.ts
+++ semantic_steve/js/src/skills/pathfind-to-coordinates.ts
@@ -1,17 +1,13 @@
 import type { Bot } from "mineflayer";
 import { SkillReturn } from "../types";
 
-/**
- * Pathfind to the given coordinates.
- * @param coords The coordinates to pathfind to.
- * @param stopIfFound The names of the things to stop at if found.
- */
 export async function pathfindToCoordinates(
   bot: Bot,
   coords: number[],
-  stopIfFound: string[],
+  stopIfFound: string[]
 ): Promise<SkillReturn> {
   return {
+    // TODO
     resultString: null,
     envStateIsUpToDate: false,
   };

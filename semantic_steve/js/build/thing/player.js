a337721bc6e2d98e10dda905e04ff210e0b88c60
Rocco A <43506958+GenerelSchwerz@users.noreply.github.com>
2025-04-16 20:12:17 -0400

    adding back build dir

--- semantic_steve/js/build/thing/player.js
+++ semantic_steve/js/build/thing/player.js
@@ -6,6 +6,12 @@ class Player {
         this.bot = bot;
         this.name = name;
     }
+    locateNearestInImmediateSurroundings() {
+        throw new Error("Method not implemented.");
+    }
+    locateNearestInDistantSurroundings(direction) {
+        throw new Error("Method not implemented.");
+    }
     locateNearest(direction) {
         throw new Error("Method not implemented.");
     }
